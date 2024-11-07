acb31720872d21523dbf6dd898cc0493b2ccd26d
pedroLucasArauj0 <pedrolucas.araujo@cadastra.com>
2024-11-07 15:16:34 -0300

    feat: setup global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import { styled } from "../styles";
 
 const Button = styled("button", {
-  backgroundColor: "$rocketseat",
-  borderRadius: 8,
+  backgroundColor: "$green300",
+  borderRadius: 4,
   border: 0,
   padding: "4px 8px",
-  color: "#fff",
+
+  span: {
+    fontWeight: "bold",
+  },
 
   "&:hover": {
     filter: "brightness(0.8)",
