1224a00bfdc66489fb8f34b614248edab3fd59e0
pedroLucasArauj0 <pedrolucas.araujo@cadastra.com>
2024-11-08 11:33:10 -0300

    feat: create sucess page structure

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,21 @@
+import Link from "next/link";
+import { ImageContainer } from "../styles/pages/product";
+import { SuccessContainer } from "../styles/pages/success";
+
 export default function Success() {
-  return <h1>Success</h1>;
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul <strong>Diego Fernandes</strong>, sua{" "}
+        <strong>Camiseta Beyond the limits</strong>
+        já está a caminho a da sua casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  );
 }
