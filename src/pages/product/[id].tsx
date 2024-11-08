f12f8ec6feb8237135f96b53ac6475a8dbc5712e
pedroLucasArauj0 <pedrolucas.araujo@cadastra.com>
2024-11-08 09:36:50 -0300

    feat: create product page layout

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,29 @@
 import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product";
+import Image from "next/future/image";
 export default function Product() {
   const { query } = useRouter();
 
-  return <h1>Product: {JSON.stringify(query)}</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer>{/* <Image /> */}</ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 100,00</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+          deleniti, quos labore officiis doloremque ad incidunt quasi aliquid
+          rem, recusandae, minus maxime architecto? Minus voluptates delectus
+          distinctio facilis magnam dolor!
+        </p>
+
+        <button>Comprar Agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
