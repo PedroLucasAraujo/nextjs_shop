142cc7a43550c41439d4c06a5c397aa0db509735
pedroLucasArauj0 <pedrolucas.araujo@cadastra.com>
2024-11-13 11:37:49 -0300

    fix: stripe version

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2022-11-15", // data sugerida de forma automática
+  apiVersion: "2024-10-28.acacia", // data sugerida de forma automática
   appInfo: {
     // informações opcionais que podemos adicionar ao realizar as requisições à api do stripe
     name: "Ignite Shop",
